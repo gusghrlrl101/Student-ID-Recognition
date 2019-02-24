c9ed63a5031308a7f1e9549e3c150e1425480b42
choiyujin <gch04750@gmail.com>
2019-02-24 17:31:58 +0900

    contour까지

--- notebook_openCV.py
+++ notebook_openCV.py
@@ -6,9 +6,9 @@ capture.set(cv2.CAP_PROP_FRAME_HEIGHT,480)  #높이
 
 while True: #영상 출력 반복
     ret, frame = capture.read()    #프레임 받아오기, ret = 상태 저장, frame = 프레임 저장
-    #cv2.imshow("VideoFrame",frame) #윈도우 창에 이미지 띄우기
+    cv2.imshow("VideoFrame",frame) #윈도우 창에 이미지 띄우기
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    cv2.imshow("gray",gray)
+    #cv2.imshow("gray",gray)
     blur = cv2.GaussianBlur(gray,(3,3),0)
     #cv2.imshow("Video",blur)
     #cv2.imwrite('blue.jpg',blur)
@@ -17,14 +17,15 @@ while True: #영상 출력 반복
     #cv2.imwrite('canny.jpg',canny)
     cv2.imshow("Canny",canny)
 
-    cnts,contours,hierarchy = cv2.findContours(canny,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    for i in range(len(contours)):
-        cnt=contours[i]
+    contours,hierarchy = cv2.findContours(canny,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
         area = cv2.contourArea(cnt)
-        x,y,w,h = cv2.boundingRect(cnt)
-        rect_area=w*h
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),1)
-        box1.append(cv2.boundingRect(cnt))
+        cv2.drawContours(frame,[cnt],-1,(0,255,0),2)
+        cv2.imshow('RGB',frame)
+        #x,y,w,h = cv2.boundingRect(cnt)
+        #rect_area=w*h
+        #cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),1)
+
 
     if cv2.waitKey(1) > 0 : break  #cv2.waitkey(time) time 마다 키 입력상태 return
                                    #키가 입력될 경우 아스키값 반환
